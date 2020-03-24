c049525b8a3e18290a2a3d35afcb11ea29e14612
Mathieu <mathieu.caselles@ynov.com>
2020-03-24 12:32:42 +0100

    retour de la navbar et materialize fonctinel

--- projet_web_b2/src/App.vue
+++ projet_web_b2/src/App.vue
@@ -1,33 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>|
-      <router-link to="/register">Register</router-link>
-    </div>
+    <nav id="nav">
+      <div class="nav-wrapper green app">
+        <a href="#" class="brand-logo green">Shop Project</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down green darken-2">
+          <li>
+            <router-link class="waves-effect waves-light btn green" to="/">
+              Accueil
+              <i class="material-icons right">home</i>
+            </router-link>
+          </li>
+          <li>
+            <router-link class="waves-effect waves-light btn green" to="/about">
+              Boutique
+              <i class="material-icons right">shopping_cart</i>
+            </router-link>
+          </li>
+          <li>
+            <router-link class="waves-effect waves-light btn green" to="/register">
+              Login
+              <i class="material-icons right">account_circle</i>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
