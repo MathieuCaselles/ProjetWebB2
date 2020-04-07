5682c4d43bbd2c55204d068b9ff1648e04de3edb
RemiFeydit <remi.feydit@ynov.com>
2020-04-07 11:17:47 +0200

    Correction conflits

--- projet_web_b2/src/views/Boutique.vue
+++ projet_web_b2/src/views/Boutique.vue
@@ -66,35 +66,6 @@ export default {
         isOk: false
     }
   },
-  data() {
-    return {
-      current: 1,
-      heroes: [
-        { name: "Wolverine", universe: "Marvel" },
-        { name: "Batman", universe: "DC" },
-        { name: "Beast", universe: "Marvel" },
-        { name: "Superman", universe: "DC" },
-        { name: "Wonder Woman", universe: "DC" },
-        { name: "Iceman", universe: "Marvel" },
-        { name: "Black Panther", universe: "Marvel" },
-        { name: "Beast Boy", universe: "DC" },
-        { name: "Captain America", universe: "Marvel" },
-        { name: "Hawkgirl", universe: "DC" },
-        { name: "Cyclops", universe: "Marvel" },
-        { name: "Green Lantern", universe: "DC" },
-        { name: "Thor", universe: "Marvel" },
-        { name: "Flash", universe: "DC" },
-        { name: "Spider-man", universe: "Marvel" },
-        { name: "Martian Manhunter", universe: "DC" },
-        { name: "Nightwing", universe: "DC" },
-        { name: "Raven", universe: "DC" },
-        { name: "Hulk", universe: "Marvel" },
-        { name: "Shehulk", universe: "Marvel" }
-      ],
-      paginate: 3,
-      paginate_total: 0
-    };
-  },
   methods: {
     setPaginate: function(i) {
       this.current = 1;
