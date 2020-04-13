a4edcb60bfbf136e4660985e38d5c20ba2057aa1
RemiFeydit <remi.feydit@ynov.com>
2020-04-13 12:42:03 +0200

    test ?

--- projet_web_b2/src/router/index.js
+++ projet_web_b2/src/router/index.js
@@ -96,6 +96,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import('../views/AdminProfile.vue')
   },
+  {
+    path: '/profil',
+    name: 'Profile',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/Profile.vue')
+  }
 ]
 
 const router = new VueRouter({
